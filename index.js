b7ed7f71ecaa2c64de4dbd1ee59d771e8e84fdd6
musin2 <muhsindaud75@gmail.com>
2024-07-21 14:37:43 +0300

    add lab code

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(res => res.json())
+  .then(jData => {
+    //console.log(jData);
+    renderBooks(jData)})
   
 }
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
